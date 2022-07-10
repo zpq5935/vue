185d7df6e6189c69ee4ec28a72d888d8ead08bca
zhangchaopei <2251745121@qq.com>
2022-07-10 13:40:52 +0800

    vue init

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import hello from '@/components/hello'
+
+Vue.use(Router)
+//获取原型对象上的push函数
+const originalPush = Router.prototype.push
+//修改原型对象中的push方法
+Router.prototype.push = function push(location) {
+   return originalPush.call(this, location).catch(err => err)
+}
+export default new Router({
+  routes: [
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/hello',
+      name: 'hello',
+      component: hello
+    }
+  ]
+})
