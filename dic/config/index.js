769bebeae9e98991701334bc60f9f3f7cee64d7f
zpq-taishi <2251745121@qq.com>
2022-07-23 20:23:49 +0800

    请求加debug日志

--- dic/config/index.js
+++ dic/config/index.js
@@ -15,6 +15,7 @@ module.exports = {
         target: 'http://localhost:38080/', // 接口的域名
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        logLevel : 'debug',
         pathRewrite: {
           '^/api': ''       //这是一个通配符，设置完了之后每个接口都要在前面加上/api（特别注意这一点）
         }
