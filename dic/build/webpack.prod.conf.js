9703da5398243a3c6c5cd4db8497af0b5807fcd8
zpq-taishi <2251745121@qq.com>
2022-07-23 20:22:49 +0800

    网站图标

--- dic/build/webpack.prod.conf.js
+++ dic/build/webpack.prod.conf.js
@@ -63,6 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
+      favicon: path.resolve('src/assets/favicon.ico'), // 引入图片地址
       inject: true,
       minify: {
         removeComments: true,
